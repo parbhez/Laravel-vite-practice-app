2d80d1c3066760aa573fa1d5d467d98975dd0b23
Mashud Parbhez <34704149+parbhez@users.noreply.github.com>
2024-11-25 15:34:16 +0600

    Inline assets 2nd part for 2nd Class

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=h2, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 vite </title>
-    {{-- @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
+    @vite('resources/js/app.js')
 </head>
 
 <style>
@@ -16,9 +16,6 @@
 </style>
 
 
-<style>
-    {!! Vite::content('resources/css/app.css') !!}
-</style>
 
 
 
@@ -26,9 +23,7 @@
 
     <h2 id="color">Hello, </h2>
 
-    <script>
-        {!! Vite::content('resources/js/app.js') !!}
-    </script>
+
 
 </body>
 
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         Inspect(), // only applies in dev mode
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/js/app.js"],
             refresh: true,
         }),
     ],
